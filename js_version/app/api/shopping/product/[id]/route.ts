a0fa1d836ca46d7bfb2264a146a43c7e5380d2a6
rokas <rokas@rokas.com>
2023-10-25 19:09:10 +0200

    Update API route and add loading state to button

--- js_version/app/api/shopping/product/[id]/route.ts
+++ js_version/app/api/shopping/product/[id]/route.ts
@@ -2,11 +2,13 @@ import {NextResponse} from "next/server";
 // import {ProductData} from "@/typings";
 
 
-const URL = "https://www.expedia.com/Hotel-Search?rooms=1&adults=2&destination=Barcelona&startDate=2023-08-16&endDate=2023-08-18&d1=2023-08-16&d2=2023-08-18"
+// const URL = "https://www.expedia.com/Hotel-Search?rooms=1&adults=2&destination=Barcelona&startDate=2023-08-16&endDate=2023-08-18&d1=2023-08-16&d2=2023-08-18"
+const URL = "https://www.expedia.com/Flights-Search?flight-type=on&mode=search&trip=roundtrip&leg1=from:Vilnius,%20Lithuania%20(VNO-Vilnius%20Intl.),to:Oslo,%20Norway%20(OSL-Gardermoen),departure:11/8/2023TANYT&leg2=from:Oslo,%20Norway%20(OSL-Gardermoen),to:Vilnius,%20Lithuania%20(VNO-Vilnius%20Intl.),departure:11/9/2023TANYT&options=cabinclass:economy&fromDate=11/8/2023&toDate=11/9/2023&d1=2023-11-8&d2=2023-11-9&passengers=adults:1,infantinlap:N";
 
 export async function GET(
   request: Request,
 ) {
+  console.log('Hello? It works from here 🔴')
   const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
     method: 'POST',
     headers: {
@@ -18,19 +20,20 @@ export async function GET(
       source: 'universal',
       url: URL,
       render: "html",
-      parse: true,
     })
   });
 
   const data = await response.json();
 
-  if(data.results.length === 0) {
-    return NextResponse.next(
-      new Response("No results", {status: 404})
-    )
-  }
+  console.log('This is the data?', data)
 
-  const productData: any = data.results[0];
+  // if(data.results.length === 0) {
+  //   return NextResponse.next(
+  //     new Response("No results", {status: 404})
+  //   )
+  // }
+  //
+  // const productData: any = data.results[0];
 
-  return NextResponse.json(productData);
+  return NextResponse.json("Hello from the server");
 }
--- js_version/app/page.tsx
+++ js_version/app/page.tsx
@@ -1,19 +1,33 @@
+"use client";
+
 import Image from 'next/image'
 import {getFetchUrl} from "@/lib/getFetchUrl";
+import {useState} from "react";
 
-export default async function Home() {
-  const response = await fetch(getFetchUrl(`api/search`), {
-    method: 'POST',
-    body: JSON.stringify({}),
-  });
-
-  const data = await response.json();
-  console.log(data);
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(getFetchUrl(`api/shopping/product/34234234234322`));
+      console.log('Fetch url is', getFetchUrl(`api/shopping/product/34234234234322`));
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+    } catch (e) {
+      console.log(`This is error 🚀`, e);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
-    <div>
-      This is the data received from the API
-      <button className="bg-red-500 px-4 py-2 rounded-md">
+    <div className="flex h-screen flex-col items-center justify-center gap-y-3">
+      <div>
+        This is the data received from the API
+      </div>
+      <button disabled={loading} onClick={fetchData} className={`bg-red-500 px-4 py-2 rounded-md ${loading && 'bg-opacity-30 bg-blue-300 cursor-not-allowed'}`}>
         This is a button to scrape
       </button>
     </div>
